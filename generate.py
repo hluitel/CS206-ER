d8239cac5f39f1e0b5212aebae9ccb9d37c8111f
hluitel <hari.luitel@uvm.edu>
2021-02-13 22:44:21 -0500

    starting the joints link

--- generate.py
+++ generate.py
@@ -8,23 +8,9 @@ height = 1
 x = 0
 y = 0
 z = 0.5
-for k in range(3):
-   y  = k
-   length = 1
-   width = 1
-   height = 1
-   for j in range(5):
-      x  = j
-      length = 1
-      width = 1
-      height = 1
-      for i in range(10): 
-         pyrosim.Send_Cube(name="Box", pos=[x,y,z+i] , size=[length,width,height])
-         length = length -.10
-         width = width - .10
-         height = height - .10
-
-
+ 
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+         
 pyrosim.End()
 
 
