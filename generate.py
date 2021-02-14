72d83e88111d05d10f3041e504932053086cc064
hluitel <hari.luitel@uvm.edu>
2021-02-13 21:53:34 -0500

    chnages

--- generate.py
+++ generate.py
@@ -1,8 +1,23 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
+length = 1
+width = 1
+height = 1
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+x = 0
+y = 0
+z = 0.5
+for k in range(3):
+   x = k
+   for j in range(3):
+      j = y
+      for i in range(10): 
+         pyrosim.Send_Cube(name="Box", pos=[x,y,z+1] , size=[length,width,height])
+         length = length -.10
+         width = width - .10
+         height = height - .10
+
 
 pyrosim.End()
 
